30335ea9d21b7cb774c2ee4ed695c54df6260653
Dhaval Salwala <dhavalsalwala@gmail.com>
2026-04-12 00:50:11 +0100

    test file removed
    Signed-off-by: Dhaval Salwala <dhavalsalwala@gmail.com>

---
+++ docs/test.py
@@ -0,0 +1,7 @@
+from ai_atlas_nexus.blocks.prompt_templates import (
+    RISK_IDENTIFICATION_PER_RISK_DSPY_TEMPLATES,
+)
+
+
+if "llama3.3:70b" in [RISK_IDENTIFICATION_PER_RISK_DSPY_TEMPLATES.keys()]:
+    print("OK")
