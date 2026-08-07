d3fbeb4957ed512b7b4ae6bd1c0edcdb812da834
Dhaval Salwala <dhavalsalwala@gmail.com>
2026-08-07 12:10:43 +0100

    fix WML API to use chat parameters
    Signed-off-by: Dhaval Salwala <dhavalsalwala@gmail.com>

--- src/ai_atlas_nexus/blocks/inference/wml.py
+++ src/ai_atlas_nexus/blocks/inference/wml.py
@@ -169,7 +169,7 @@ class WMLInferenceEngine(InferenceEngine):
     def generate_chat_response(
         self, response_format, tools, messages
     ) -> List[TextGenerationInferenceOutput]:
-        self.parameters.update({"guided_json": self.format(response_format}))
+        self.parameters.update({"guided_json": self.format(response_format)})
         return self.client.chat(
             messages=self._to_openai_format(messages),
             params=self.parameters,
